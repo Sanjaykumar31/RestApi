1b7cc663171b9a2f90bd238897319dd04a2df82f
Sanjay Kumar <sanjayamudha2001@gmail.com>
2022-10-26 11:43:26 +0530

    rollback

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
-app.use('/studentRouter', studentRouter)
+app.use('/studentrouter', studentRouter)
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 
 
